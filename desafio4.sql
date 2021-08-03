b03ecd14f2ed756cdc5b9a33ed6269ae9fdc3442
Felippe <felippecorrea2010@gmail.com>
2021-08-03 19:00:45 -0300

    DONE: desafio4.sql

---
+++ desafio4.sql
@@ -0,0 +1,1 @@
+CREATE VIEW top_3_artistas AS SELECT a.nome_artista AS 'artista', COUNT(s.artista_id) AS 'seguidores' FROM SpotifyClone.artista AS a INNER JOIN SpotifyClone.seguidor AS s ON a.artista_id = s.artista_id GROUP BY s.artista_id ORDER BY COUNT(s.artista_id) DESC LIMIT 3;
