8ce086d45e989a0a64727d7059c7ae6bd18bc5ee
Felippe <felippecorrea2010@gmail.com>
2021-08-03 21:09:04 -0300

    DONE: desafio9.sql

---
+++ desafio9.sql
@@ -0,0 +1,1 @@
+DELIMITER $$; CREATE PROCEDURE albuns_do_artista(IN artista VARCHAR(50)) BEGIN SELECT a.nome_artista AS 'artista', al.nome_album AS 'album' FROM SpotifyClone.artista AS a INNER JOIN SpotifyClone.album AS al ON a.artista_id = al.artista_id WHERE a.nome_artista = artista ORDER BY al.nome_album; END $$; DELIMITER ; CALL albuns_do_artista('Walter Phoenix');
